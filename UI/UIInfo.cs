23e85eb1bc65239201d15dc61cce76248d896c40
SaerusTierialis <me12399@gmail.com>
2018-11-04 22:15:45 -0500

    info panle now slightly less ugly (added title)

--- UI/UIInfo.cs
+++ UI/UIInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,38 +17,56 @@ namespace ExperienceAndClasses.UI {
         public static readonly UIInfo Instance = new UIInfo();
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        private const float TEXT_SCALE = 1;
+        private const float TEXT_SCALE_TITLE = 1;
+        private const float TEXT_SCALE_BODY = 0.7f;
 
         private const float WIDTH_CLASS = 300;
 
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        private DragableUIPanel panel;
-        private UIText ui_text;
+        private DragableUIPanel panel_title;
+        private DragableUIPanel panel_body;
+        private UIText ui_text_title;
+        private UIText ui_text_body;
         private UIElement source = null;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Initialize ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         protected override void InitializeState() {
-            panel = new DragableUIPanel(1f, 1f, Shared.COLOR_UI_PANEL_BACKGROUND, this, false, false, false);
+            panel_title = new DragableUIPanel(1f, 1f, Shared.COLOR_UI_PANEL_BACKGROUND, this, false, false, false);
+            panel_body = new DragableUIPanel(1f, 1f, Shared.COLOR_UI_PANEL_BACKGROUND, this, false, false, false);
 
-            ui_text = new UIText("", TEXT_SCALE, false);
-            ui_text.Left.Set(Shared.UI_PADDING, 0f);
-            ui_text.Top.Set(Shared.UI_PADDING, 0f);
-            panel.Append(ui_text);
+            ui_text_title = new UIText("", TEXT_SCALE_TITLE, false);
+            ui_text_title.Left.Set(0f, 0f);
+            ui_text_title.Top.Set(Shared.UI_PADDING, 0f);
+            panel_title.Append(ui_text_title);
 
-            state.Append(panel);
+            ui_text_body = new UIText("", TEXT_SCALE_BODY, false);
+            ui_text_body.Left.Set(Shared.UI_PADDING, 0f);
+            ui_text_body.Top.Set(Shared.UI_PADDING, 0f);
+            panel_body.Append(ui_text_body);
+
+            state.Append(panel_body);
+            state.Append(panel_title);
         }
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        private void ShowText(UIElement source, string text, float width) {
+        private void ShowText(UIElement source, string title, string body, float width) {
             if ((this.source == null) || !this.source.Equals(source)) {
                 this.source = source;
-                panel.SetPosition(source.GetDimensions().X + source.Width.Pixels, source.GetDimensions().Y);
 
-                text = Main.fontMouseText.CreateWrappedText(text, (width - Shared.UI_PADDING*2) / TEXT_SCALE);
-                panel.SetSize(width, Main.fontMouseText.MeasureString(text).Y*TEXT_SCALE);
+                //title
+                panel_title.SetPosition(source.GetDimensions().X + source.Width.Pixels, source.GetDimensions().Y);
+                Vector2 measure_title = Main.fontMouseText.MeasureString(title);
+                panel_title.SetSize(width, measure_title.Y * TEXT_SCALE_TITLE);
+                ui_text_title.Left.Set((width - (measure_title.X*TEXT_SCALE_TITLE)) / 2f, 0f);
+                ui_text_title.SetText(title);
 
-                ui_text.SetText(text);
+                //body
+                panel_body.SetPosition(panel_title.Left.Pixels, panel_title.Top.Pixels);
+                body = Main.fontMouseText.CreateWrappedText(body, (width - Shared.UI_PADDING*2) / TEXT_SCALE_BODY);
+                panel_body.SetSize(width, (Main.fontMouseText.MeasureString(body).Y*TEXT_SCALE_BODY) + panel_title.Height.Pixels + Shared.UI_PADDING);
+                ui_text_body.Top.Set(Shared.UI_PADDING + panel_title.Height.Pixels, 0f);
+                ui_text_body.SetText(body);
 
                 Visibility = true;
             }
@@ -76,17 +95,15 @@ namespace ExperienceAndClasses.UI {
         public void ShowTextClass(UIElement source, byte class_id) {
             Systems.Class c = Systems.Classes.CLASS_LOOKUP[class_id];
 
-            string text = c.Name.ToUpper();
+            string title = c.Name.ToUpper();
+            string text = "";
             if (ExperienceAndClasses.LOCAL_MPLAYER.class_levels[class_id] <= 0) {
-                text += " (locked)\n\n";
+                title += " (locked)";
                 text += "Requirement: " + c.GetPrereqString() + "\n\n";
-            }
-            else {
-                text += "\n\n";
             }
             text += c.GetAttributeString() + "\n\n" + c.Description;
 
-            ShowText(source, text, WIDTH_CLASS);
+            ShowText(source, title, text, WIDTH_CLASS);
         }
 
     }
