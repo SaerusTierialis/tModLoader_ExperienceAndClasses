a6d715109881fd66fd67fd0bbb9c760a4499191b
SaerusTierialis <me12399@gmail.com>
2018-11-09 23:56:42 -0500

    start xp

--- MNPC.cs
+++ MNPC.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ExperienceAndClasses {
     class MNPC : GlobalNPC {
 
-
-
-        public override void SetDefaults(NPC npc) {
-            
+        public override void NPCLoot(NPC npc) {
+            base.NPCLoot(npc);
+            double xp = Systems.XP.CalcBaseExp(npc);
         }
 
     }
---
+++ Systems/XP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace ExperienceAndClasses.Systems {
+    class XP {
+
+        public static double CalcBaseExp(NPC npc) {
+            //no exp from statue, critter, or friendly
+            if (npc.SpawnedFromStatue || npc.lifeMax <= 5 || npc.friendly) return 0f;
+
+            float experience = 0; ;
+            if (npc.defDefense >= 1000)
+                experience = (npc.lifeMax / 100f) * (1f + (npc.defDamage / 25f));
+            else
+                experience = (npc.lifeMax / 100f) * (1f + (npc.defDefense / 10f)) * (1f + (npc.defDamage / 25f));
+
+            return experience;
+        }
+
+    }
+}
