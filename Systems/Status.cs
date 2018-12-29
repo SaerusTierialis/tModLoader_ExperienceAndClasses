09917ec8f2b81d79e6727c13aeff44b96be2a5e6
SaerusTierialis <me12399@gmail.com>
2018-12-28 23:02:18 -0500

    refactor status + finished AddPacket

--- Systems/Status.cs
+++ Systems/Status.cs
@@ -34,16 +34,20 @@ namespace ExperienceAndClasses.Systems {
     public abstract class Status {
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IDs (order does not matter) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-        public enum IDs : short {
+        public enum IDs : ushort {
             Heal,
 
             //insert here
 
-            NUMBER_OF_IDs, //leave this last
+            NUMBER_OF_IDs //leave this last
         }
 
         protected enum SYNC_DATA_TYPES : byte {
             MAGNITUDE,
+
+            //insert here
+
+            NUMBER_OF_TYPES //leave this last
         }
         protected static readonly IEnumerable<SYNC_DATA_TYPES> SYNC_DATA_TYPES_STRINGS = Enum.GetValues(typeof(SYNC_DATA_TYPES)).Cast< SYNC_DATA_TYPES>();
 
@@ -52,7 +56,7 @@ namespace ExperienceAndClasses.Systems {
         public static Status[] LOOKUP { get; private set; }
 
         static Status() {
-            LOOKUP = new Status[(short)Status.IDs.NUMBER_OF_IDs];
+            LOOKUP = new Status[(ushort)Status.IDs.NUMBER_OF_IDs];
             string[] IDs = Enum.GetNames(typeof(IDs));
             for (byte i = 0; i < LOOKUP.Length; i++) {
                 LOOKUP[i] = (Status)(Assembly.GetExecutingAssembly().CreateInstance(typeof(Status).FullName + "+" + IDs[i]));
@@ -61,24 +65,26 @@ namespace ExperienceAndClasses.Systems {
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-        //ID (must override)
-        public abstract IDs ID();
-        public abstract short ID_num();
+        //ID
+        public IDs ID { get; private set; }
+        public ushort ID_num { get; private set; }
 
-        //core info (can override)
-        protected virtual string Core_Texture_Path_Buff { get { return null; } }
-        public virtual bool Core_Duration_Instant { get { return false; } }
-        public virtual bool Core_Duration_Unlimited { get { return false; } }
-        public virtual uint Core_Duration_msec { get { return 0; } }
-        public virtual bool Core_Sync { get { return true; } }
-        public virtual bool Core_Show_Buff { get { return true; } }
-        public virtual string Core_Name { get { return "Undefined"; } }
-        public virtual string Core_Description { get { return "Undefined"; } }
+        //core info (overwrite default as needed)
+        protected string core_texture_path_buff;
+        protected bool core_duration_instant;
+        protected bool core_duration_unlimited;
+        protected uint core_duration_msec;
+        protected bool core_sync;
+        public bool core_show_buff { get; protected set; }
+        public string core_display_name { get; protected set; }
+        public string core_description { get; protected set; }
+        protected List<SYNC_DATA_TYPES> core_sync_data_types;
 
         //public variables
         public MPlayer Owner { get; protected set; }
         public MPlayer Target { get; protected set; }
-        public double Percent_Remaining { get; protected set; } //track duration on local player only
+        public double Time_Remaining_Percent { get; protected set; } //track duration on local player only
+        public uint Time_Remaining_msec { get; protected set; }
 
         //protected variables
         protected Dictionary<SYNC_DATA_TYPES, double> sync_data;
@@ -94,7 +100,7 @@ namespace ExperienceAndClasses.Systems {
                     return texture_buff;
                 }
                 else {
-                    return LOOKUP[ID_num()].Texture_Buff;
+                    return LOOKUP[ID_num].Texture_Buff;
                 }
             }
             protected set {
@@ -102,10 +108,25 @@ namespace ExperienceAndClasses.Systems {
             }
         }
 
-        public Status() {
+        public Status(IDs id) {
+            //ID
+            ID = id;
+            ID_num = (ushort)id;
+
+            //core info defaults
+            core_display_name = "Undefined";
+            core_description = "Undefined";
+            core_show_buff = false;
+            core_duration_instant = false;
+            core_duration_unlimited = false;
+            core_duration_msec = 5000;
+            core_texture_path_buff = null;
+            core_sync = true;
+            core_sync_data_types = new List<SYNC_DATA_TYPES>();
+
             //defaults
             time_end = DateTime.MinValue;
-            Percent_Remaining = 100;
+            Time_Remaining_Percent = 100;
             Owner = null;
             Target = null;
             sync_data = new Dictionary<SYNC_DATA_TYPES, double>();
@@ -116,11 +137,11 @@ namespace ExperienceAndClasses.Systems {
             if (Target.player.whoAmI == Main.LocalPlayer.whoAmI) {
                 //update remaining time
                 if (HasDuration()) {
-                    Percent_Remaining = time_end.Subtract(DateTime.Now).TotalMilliseconds / Core_Duration_msec * 100;
+                    Time_Remaining_Percent = time_end.Subtract(DateTime.Now).TotalMilliseconds / core_duration_msec * 100;
                 }
 
                 //check if needs to end
-                if ((Percent_Remaining < 0) || !Main.player[Owner.player.whoAmI].Equals(Owner.player)) {
+                if ((Time_Remaining_Percent < 0) || !Main.player[Owner.player.whoAmI].Equals(Owner.player)) {
                     Remove();
                     return;
                 }
@@ -128,9 +149,9 @@ namespace ExperienceAndClasses.Systems {
             OnUpdate();
         }
 
-        protected static void Add(Player target, MPlayer owner, Type type, Dictionary<SYNC_DATA_TYPES, double> sync_data) {
+        protected static void Add(Player target, MPlayer owner, IDs ID, Dictionary<SYNC_DATA_TYPES, double> sync_data) {
             //create new instance
-            Status status = (Status)(Assembly.GetExecutingAssembly().CreateInstance(type.FullName));
+            Status status = (Status)(Assembly.GetExecutingAssembly().CreateInstance(typeof(Status).FullName + "+" + ID));
             
             //set sync_data
             if (sync_data != null) {
@@ -143,12 +164,12 @@ namespace ExperienceAndClasses.Systems {
 
             //calculate end time if there is one
             if (status.HasDuration()) {
-                status.time_end = DateTime.Now.AddMilliseconds(status.Core_Duration_msec);
+                status.time_end = DateTime.Now.AddMilliseconds(status.core_duration_msec);
             }
 
             //add to target (unless instant)
-            if (!status.Core_Duration_Instant) {
-                Status prior_instance = status.Target.Status[status.ID_num()];
+            if (!status.core_duration_instant) {
+                Status prior_instance = status.Target.Status[status.ID_num];
                 if (prior_instance != null) {
                     if (!prior_instance.TryMerge(status)) {
                         //don't add if cannot merge to existing
@@ -156,13 +177,13 @@ namespace ExperienceAndClasses.Systems {
                     }
                 }
                 else {
-                    status.Target.Status[status.ID_num()] = status;
+                    status.Target.Status[status.ID_num] = status;
                     status.OnStart();
                 }
             }
 
             //if owner is local and netmode is multiplayer, send to other clients (unless not sync)
-            if (status.Core_Sync && ExperienceAndClasses.IS_CLIENT && status.Owner.Equals(ExperienceAndClasses.LOCAL_MPLAYER)) {
+            if (status.core_sync && ExperienceAndClasses.IS_CLIENT && status.Owner.Equals(ExperienceAndClasses.LOCAL_MPLAYER)) {
                 status.SendAddPacket();
             }
         }
@@ -180,10 +201,8 @@ namespace ExperienceAndClasses.Systems {
         }
 
         public void Remove() {
-            //remove from player (unless it was instant)
-            if (!Core_Duration_Instant) {
-                Target.Status[ID_num()] = null;
-            }
+            //remove from player
+            Target.Status[ID_num] = null;
 
 
 
@@ -207,36 +226,49 @@ namespace ExperienceAndClasses.Systems {
         }
 
         /// <summary>
-        /// 1:  ID (int32)
+        /// 1:  ID (ushort = uint16)
         /// 2:  Owner (byte)
         /// 3:  Target (byte)
-        /// 4:  number of extra data values, can be 0 (byte)
-        /// 5+: extra data values in enum order  (double)
+        /// 4+: extra data values in enum order  (double[])
         /// </summary>
         /// <param name="packet"></param>
         private void WriteAddPacketBody(ModPacket packet) {
-            packet.Write(ID_num());
+            //1:  ID (ushort = uint16)
+            packet.Write(ID_num);
+
+            //2:  Owner (byte)
             packet.Write((byte)Owner.player.whoAmI);
+
+            //3:  Target (byte)
             packet.Write((byte)Target.player.whoAmI);
 
-            //gather data
-            List<double> data = new List<double>();
-            double value;
-            foreach (SYNC_DATA_TYPES type in SYNC_DATA_TYPES_STRINGS) {
-                if (sync_data.TryGetValue(type, out value)) {
-                    data.Add(value);
-                }
-            }
-
-            //write data
-            packet.Write((byte)data.Count);
-            foreach(double d in data) {
-                packet.Write(d);
+            //4+: extra data values in enum order  (double[])
+            foreach (SYNC_DATA_TYPES type in core_sync_data_types) {
+                packet.Write(GetData(type));
             }
         }
 
         public void ReadAddPacketBody(BinaryReader reader) {
-            //TODO
+            //1:  ID (ushort = uint16)
+            int id_num = reader.ReadUInt16();
+            IDs id = LOOKUP[id_num].ID;
+
+            //2:  Owner (byte)
+            byte owner_byte = reader.ReadByte();
+            MPlayer owner = Main.player[owner_byte].GetModPlayer<MPlayer>();
+
+            //3:  Target (byte)
+            byte target_byte = reader.ReadByte();
+            Player target = Main.player[target_byte];
+
+            //4+: extra data values in enum order  (double[])
+            Dictionary<SYNC_DATA_TYPES, double> data = new Dictionary<SYNC_DATA_TYPES, double>();
+            foreach (SYNC_DATA_TYPES type in core_sync_data_types) {
+                data.Add(type, reader.ReadDouble());
+            }
+
+            //Add status
+            Add(target, owner, ID, data);
         }
 
         /// <summary>
@@ -252,32 +284,17 @@ namespace ExperienceAndClasses.Systems {
             //TODO
         }
 
-        /// <summary>
-        /// 1:  ID (int32)
-        /// 2:  Owner (byte)
-        /// 3:  number of extra data values, can be 0 (byte)
-        /// 4+: extra data values in enum order  (double)
-        /// </summary>
-        /// <param name="packet"></param>
-        public void WriteSetPacketBody(ModPacket packet) {
-            //TODO
-        }
-
-        public void ReadSetPacketBody(BinaryReader reader, MPlayer target) {
-            //TODO
-        }
-
         public void LoadTexture() {
-            if (Core_Texture_Path_Buff != null) {
-                Texture_Buff = ModLoader.GetTexture(Core_Texture_Path_Buff);
+            if (core_texture_path_buff != null) {
+                Texture_Buff = ModLoader.GetTexture(core_texture_path_buff);
             }
             else {
                 Texture_Buff = Textures.TEXTURE_STATUS_DEFAULT;
             }
         }
 
-        protected bool HasDuration() {
-            return (!Core_Duration_Instant && !Core_Duration_Unlimited);
+        public bool HasDuration() {
+            return (!core_duration_instant && !core_duration_unlimited);
         }
 
         //override with any specific effects
@@ -287,34 +304,33 @@ namespace ExperienceAndClasses.Systems {
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Shortcuts to Sync Data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-        public double Magitude() {
-            return GetData(this, SYNC_DATA_TYPES.MAGNITUDE);
-        }
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Static Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-        protected static double GetData(Status status, SYNC_DATA_TYPES key, double default_value=-1) {
+        protected double GetData(SYNC_DATA_TYPES key, double default_value = -1) {
             double value = default_value;
-            if (!status.sync_data.TryGetValue(key, out value)) {
-                Commons.Error("Status attempted to access invalid sync data: " + status.GetType() + " " + key);
+            if (!sync_data.TryGetValue(key, out value)) {
+                Commons.Error("Status attempted to access invalid sync data: " + GetType() + " " + key);
             }
             return value;
         }
 
+        public double Magitude { get { return GetData(SYNC_DATA_TYPES.MAGNITUDE); } }
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Static Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Example ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         public class Heal : Status {
-            //must override ID
-            public override IDs ID() { return IDs.Heal; }
-            public override short ID_num() { return (short)ID(); }
+            public Heal() : base(IDs.Heal) {
+                //any overwrites
+                core_display_name = "Heal"; //not needed unless displayed as a buff
+                core_texture_path_buff = "ExperienceAndClasses/Textures/Status/Heal"; //not needed unless displayed as a buff
 
-            //may override any core info
-            protected override string Core_Texture_Path_Buff { get { return "ExperienceAndClasses/Textures/Status/Heal"; } }
+                //add any sync data types that will be used (for syncing)
+                core_sync_data_types.Add(SYNC_DATA_TYPES.MAGNITUDE);
+            }
 
-            //must inlcude a static add method which creates a new instance, adds any extra data, and then adds it to the target
-            //if there is no sync_data, just pass null
+            //must inlcude a static add method with target/owner and any extra info
             public static void Add(Player target, MPlayer owner, double magnitude) {
-                Add(target, owner, typeof(Heal), new Dictionary<SYNC_DATA_TYPES, double> {
+                Add(target, owner, IDs.Heal, new Dictionary<SYNC_DATA_TYPES, double> {
                     { SYNC_DATA_TYPES.MAGNITUDE, magnitude }
                 });
             }
