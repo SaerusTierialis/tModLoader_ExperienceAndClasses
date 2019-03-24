7bcbb0a9563e98085fb3c87bcc1a2b1851917e8c
SaerusTierialis <me12399@gmail.com>
2019-03-24 12:44:39 -0400

    create searches (todo)

---
+++ Utilities/Search.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperienceAndClasses.Utilities {
+    static class Search {
+        public static void FindPlayersInRange(Vector2 position, float range, bool line_of_sight=false) {
+            //TODO
+        }
+    }
+}
