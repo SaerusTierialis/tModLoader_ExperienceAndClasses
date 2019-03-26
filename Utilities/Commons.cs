01b3a5012b8531ea0d8e9ce68630e8085b831b74
SaerusTierialis <me12399@gmail.com>
2019-03-25 22:42:04 -0400

    remove code for old tModLoader

--- Utilities/Commons.cs
+++ Utilities/Commons.cs
@@ -107,30 +107,6 @@ namespace ExperienceAndClasses.Utilities {
             else {
                 return defaultValue;
             }
-
-            /*
-             * This method no longer works because tag get methods no long throw exceptions when name or type is wrong
-             * 
-            try {
-                T val;
-                Type type = typeof(T);
-                if (type == typeof(int)) val = (T)Convert.ChangeType(tag.GetInt(key), type);
-                else if (type == typeof(float)) val = (T)Convert.ChangeType(tag.GetFloat(key), type);
-                else if (type == typeof(double)) val = (T)Convert.ChangeType(tag.GetDouble(key), type);
-                else if (type == typeof(bool)) val = (T)Convert.ChangeType(tag.GetBool(key), type);
-                else if (type == typeof(long)) val = (T)Convert.ChangeType(tag.GetLong(key), type);
-                else if (type == typeof(string)) val = (T)Convert.ChangeType(tag.GetString(key), type);
-                else if (type == typeof(byte)) val = (T)Convert.ChangeType(tag.GetByte(key), type);
-                else if (type == typeof(byte[])) val = (T)Convert.ChangeType(tag.GetByteArray(key), type);
-                else if (type == typeof(int[])) val = (T)Convert.ChangeType(tag.GetIntArray(key), type);
-                else throw new Exception();
-
-                return val;
-            }
-            catch {
-                return defaultValue;
-            }
-            */
         }
 
         public static void Error(string message) {
