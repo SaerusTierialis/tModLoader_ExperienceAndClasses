cdb94d65a0850b20b66f5d2ddc901e389f1fc6b0
SaerusTierialis <me12399@gmail.com>
2019-04-01 01:10:27 -0400

    NOT COMPILING YET - started final status core

--- Systems/Status.cs
+++ Systems/Status.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -10,21 +9,6 @@ using Terraria;
 using Terraria.ModLoader;
 
 namespace ExperienceAndClasses.Systems {
-    /*
-     * TODO check if this is still accurate after changes
-     * 
-     * Statuses are like the builtin buff system, but can carry any amount of extra data. The system exists because
-     * there is a hard limit on the number of buffs that a player can have at one time and the buff system is limited
-     * in the amount of data that is automatically synced.
-     * 
-     * Statuses can be used for instant effects, toggles, duration buffs, and much more. They can even be displayed with
-     * buffs in the top left UI. Even abilities such as the support class' Sanctuary can be implemented as a status (with
-     * a location in the sync data).
-     * 
-     * Statuses have builtin syncing. Duration checking is handled by the client who is targeted by the status.
-     * 
-     * The lookup instances cannot be reused. A new instance must be created when a status is added to a player.
-    */
     public abstract class Status {
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ IDs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -36,14 +20,66 @@ namespace ExperienceAndClasses.Systems {
             NUMBER_OF_IDs //leave this last
         }
 
-        protected enum SYNC_DATA_TYPES : byte {
-            MAGNITUDE,
+        /// <summary>
+        /// Auto-syncing data types available (always float)
+        /// </summary>
+        protected enum AUTOSYNC_DATA_TYPES : byte {
+            MAGNITUDE1,
+            MAGNITUDE2,
+            RANGE,
 
             //insert here
 
             NUMBER_OF_TYPES //leave this last
         }
-        protected static readonly IEnumerable<SYNC_DATA_TYPES> SYNC_DATA_TYPES_STRINGS = Enum.GetValues(typeof(SYNC_DATA_TYPES)).Cast< SYNC_DATA_TYPES>();
+        protected static readonly IEnumerable<AUTOSYNC_DATA_TYPES> SYNC_DATA_TYPES_STRINGS = Enum.GetValues(typeof(AUTOSYNC_DATA_TYPES)).Cast<AUTOSYNC_DATA_TYPES>();
+
+        protected enum DURATION_TYPES : byte {
+            INSTANT,
+            TIMED,
+            TOGGLE,
+        }
+
+        /// <summary>
+        /// Is there an OnUpdate effect? Is there a repeating timed effect?
+        /// </summary>
+        protected enum EFFECT_TYPES : byte {
+            NONE,
+            CONSTANT,
+            TIMED,
+        }
+
+        /// <summary>
+        /// Limit on how many instances can be on a single target
+        /// </summary>
+        protected enum LIMIT_TYPES : byte {
+            UNLIMITED,
+            ONE_PER_OWNER,
+            ONE,
+        }
+
+        /// <summary>
+        /// Which instances to apply if there are multiple
+        /// </summary>
+        protected enum APPLY_TYPES : byte {
+            ALL,
+            BEST_PER_OWNER,
+            BEST,
+        }
+
+        /// <summary>
+        /// Which instances to show in UI
+        /// </summary>
+        protected enum UI_TYPES : byte {
+            NONE,
+            ONE,
+            ALL_APPLY,
+            ALL,
+        }
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        private const int TEXTURE_INDEX_NONE = -1;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Auto-Populated Lookup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -57,283 +93,158 @@ namespace ExperienceAndClasses.Systems {
             }
         }
 
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Static ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        private static List<Texture2D> Textures = new List<Texture2D>();
+
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         //ID
         public IDs ID { get; private set; }
         public ushort ID_num { get; private set; }
+        public byte Instance_ID { get; private set; }
 
-        //core info (overwrite default as needed)
-        protected string core_texture_path_buff;
-        protected bool core_duration_instant;
-        protected bool core_duration_unlimited;
-        protected uint core_duration_msec;
-        protected bool core_sync;
-        public bool core_show_buff { get; protected set; }
-        public string core_display_name { get; protected set; }
-        public string core_description { get; protected set; }
-        protected List<SYNC_DATA_TYPES> core_sync_data_types;
+        //status-specific vars
+        protected string specific_texture_path, specific_name, specific_description;
+        protected List<AUTOSYNC_DATA_TYPES> specific_autosync_data_types;
+        protected bool specific_target_can_be_player, specific_target_can_be_npc, specific_owner_can_be_player, specific_owner_can_be_npc;
+        protected DURATION_TYPES specific_duration_type;
+        protected float specific_duration_sec;
+        protected EFFECT_TYPES specific_effect_type;
+        protected float specific_effect_update_frequency_sec;
+        protected LIMIT_TYPES specific_limit_type;
+        protected APPLY_TYPES specific_apply_type;
+        protected bool specific_allow_merge; //when a status would overwrite another, merge them instead (use max duration, magnitude, etc. - use new owner)
+        protected bool specific_syncs;
+        protected UI_TYPES specific_ui_type;
+        protected Systems.Class.IDs specific_owner_required_class;
+        protected byte specific_owner_required_class_level;
+        protected IDs specific_owner_required_status;
+        protected IDs specific_target_required_status;
 
-        //public variables
-        public MPlayer Owner { get; protected set; }
-        public MPlayer Target { get; protected set; }
-        public double Time_Remaining_Percent { get; protected set; } //track duration on local player only
-        public uint Time_Remaining_msec { get; protected set; }
+        //target
+        protected bool target_is_player; //else NPC
+        protected MPlayer target_mplayer;
+        protected MNPC target_MNPC;
 
-        //protected variables
-        protected Dictionary<SYNC_DATA_TYPES, double> sync_data;
+        //owner
+        protected bool owner_is_player; //else NPC
+        protected MPlayer owner_mplayer;
+        protected MNPC owner_MNPC;
 
-        //private variables
-        private Texture2D texture_buff;
-        private DateTime time_end;
+        //generic
+        private DateTime time_end, time_next_effect;
+        protected bool locally_owned; //the local client is the owner (always false for server)
+        private bool check_duration; //is this client (or server) responsible for enforcing duration
+        protected Dictionary<AUTOSYNC_DATA_TYPES, float> autosync_data;
 
-        //public instance_id (don't touch this except in StatusList)
-        public byte instance_id;
+        //for UI
+        public string Time_Remaining { get; private set; }
+        protected ushort time_remaining_min, time_remaining_sec;
 
-        //get texture from any instance
-        public Texture2D Texture_Buff {
-            get {
-                if (texture_buff != null) {
-                    return texture_buff;
-                }
-                else {
-                    return LOOKUP[ID_num].Texture_Buff;
-                }
-            }
-            protected set {
-                texture_buff = value;
-            }
-        }
+        //set during init
+        private int texture_index; 
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         public Status(IDs id) {
             //ID
             ID = id;
             ID_num = (ushort)id;
+            Instance_ID = Utilities.Containers.StatusList.UNASSIGNED_INSTANCE_KEY;
+            
+            //status-specific defaults
+            specific_name = "default_status";
+            specific_description = "default_description";
+            specific_autosync_data_types = new List<AUTOSYNC_DATA_TYPES>();
+            specific_target_can_be_player = specific_target_can_be_npc = specific_owner_can_be_player = specific_owner_can_be_npc = true;
+            specific_duration_type = DURATION_TYPES.TOGGLE;
+            specific_duration_sec = -1;
+            specific_effect_type = EFFECT_TYPES.NONE;
+            specific_effect_update_frequency_sec = -1;
+            specific_limit_type = LIMIT_TYPES.UNLIMITED;
+            specific_apply_type = APPLY_TYPES.ALL;
+            specific_allow_merge = true;
+            specific_syncs = true;
+            specific_ui_type = UI_TYPES.ALL;
 
-            //core info defaults
-            core_display_name = "Undefined";
-            core_description = "Undefined";
-            core_show_buff = false;
-            core_duration_instant = false;
-            core_duration_unlimited = false;
-            core_duration_msec = 5000;
-            core_texture_path_buff = null;
-            core_sync = true;
-            core_sync_data_types = new List<SYNC_DATA_TYPES>();
+            //generic
+            autosync_data = new Dictionary<AUTOSYNC_DATA_TYPES, float>();
 
-            //defaults
-            time_end = DateTime.MinValue;
-            Time_Remaining_Percent = 100;
-            Owner = null;
-            Target = null;
-            sync_data = new Dictionary<SYNC_DATA_TYPES, double>();
-            instance_id = Utilities.Containers.StatusList.UNASSIGNED_INSTANCE_KEY;
+            //texture
+            texture_index = TEXTURE_INDEX_NONE;
         }
 
-        public void Update() {
-            //target of status ends it if out of time or owner has left //TODO or if owner no longer has source ability
-            if (Target.player.whoAmI == Main.LocalPlayer.whoAmI) {
-                //update remaining time
-                if (HasDuration()) {
-                    Time_Remaining_Percent = time_end.Subtract(DateTime.Now).TotalMilliseconds / core_duration_msec * 100;
-                }
-
-                //check if needs to end
-                if ((Time_Remaining_Percent < 0) || !Main.player[Owner.player.whoAmI].Equals(Owner.player)) {
-                    Remove();
-                    return;
-                }
-            }
-            OnUpdate();
-        }
-
-        protected static void Add(Player target, MPlayer owner, IDs ID, Dictionary<SYNC_DATA_TYPES, double> sync_data) {
-            //create new instance
-            Status status = (Status)LOOKUP[(ushort)ID].MemberwiseClone();
-
-            //set sync_data
-            if (sync_data != null) {
-                status.sync_data = sync_data;
-            }
-
-            //set
-            status.Target = target.GetModPlayer<MPlayer>();
-            status.Owner = owner;
-
-            //calculate end time if there is one
-            if (status.HasDuration()) {
-                status.time_end = DateTime.Now.AddMilliseconds(status.core_duration_msec);
-            }
-
-            //TODO
-
-            /*
-            //add to target (unless instant)
-            if (!status.core_duration_instant) {
-                Status prior_instance = status.Target.Status[status.ID_num];
-                if (prior_instance != null) {
-                    if (!prior_instance.TryMerge(status)) {
-                        //don't add if cannot merge to existing
-                        return;
-                    }
-                }
-                else {
-                    status.Target.Status[status.ID_num] = status;
-                    status.OnStart();
-                }
-            }
-            */
-
-            //if owner is local and netmode is multiplayer, send to other clients (unless not sync)
-            if (status.core_sync && Utilities.Netmode.IS_CLIENT && status.Owner.Equals(ExperienceAndClasses.LOCAL_MPLAYER)) {
-                status.SendAddPacket();
-            }
-        }
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         /// <summary>
-        /// merge two instances of a status keeping the highest values
-        /// return false if nothing other than 
+        /// Run one during init
         /// </summary>
-        /// <param name="status"></param>
-        /// <returns></returns>
-        protected bool TryMerge(Status status) {
-            //TODO (merge, keep highest values)
-            //dont merge if the only thing that has changed is duration and the duration has changed by less than 0.5 sec
-            return true;
-        }
-
-        public void Remove() {
-            //remove from player
-            Target.Statuses.RemoveStatus(this);
-
-            //TODO
-
-            OnEnd();
-        }
-
-        private void SendAddPacket(int origin=-1) {
-            //origin is local player unless told otherwise
-            if (origin == -1) {
-                origin = Main.LocalPlayer.whoAmI;
-            }
-
-            //create packet
-            ModPacket packet = Utilities.PacketHandler.AddStatus.Instance.GetPacket(origin);
-
-            //fill packet
-            WriteAddPacketBody(packet);
-
-            //send packet
-            packet.Send(-1, origin);
-        }
-
-        /// <summary>
-        /// 1:  ID (ushort = uint16)
-        /// 2:  Owner (byte)
-        /// 3:  Target (byte)
-        /// 4+: extra data values in enum order  (double[])
-        /// </summary>
-        /// <param name="packet"></param>
-        private void WriteAddPacketBody(ModPacket packet) {
-            //1:  ID (ushort = uint16)
-            packet.Write(ID_num);
-
-            //2:  Owner (byte)
-            packet.Write((byte)Owner.player.whoAmI);
-
-            //3:  Target (byte)
-            packet.Write((byte)Target.player.whoAmI);
-
-            //4+: extra data values in enum order  (double[])
-            foreach (SYNC_DATA_TYPES type in core_sync_data_types) {
-                packet.Write(GetData(type));
-            }
-        }
-
-        public void ReadAddPacketBody(BinaryReader reader) {
-            //1:  ID (ushort = uint16)
-            int id_num = reader.ReadUInt16();
-            IDs id = LOOKUP[id_num].ID;
-
-            //2:  Owner (byte)
-            byte owner_byte = reader.ReadByte();
-            MPlayer owner = Main.player[owner_byte].GetModPlayer<MPlayer>();
-
-            //3:  Target (byte)
-            byte target_byte = reader.ReadByte();
-            Player target = Main.player[target_byte];
-
-            //4+: extra data values in enum order  (double[])
-            Dictionary<SYNC_DATA_TYPES, double> data = new Dictionary<SYNC_DATA_TYPES, double>();
-            foreach (SYNC_DATA_TYPES type in core_sync_data_types) {
-                data.Add(type, reader.ReadDouble());
-            }
-
-            //Add status
-            Add(target, owner, ID, data);
-        }
-
-        /// <summary>
-        /// 1:  ID (int32)
-        /// 2:  Owner (byte)
-        /// 3:  Target (byte)
-        /// </summary>
-        private void SendRemovePacket() {
-            //TODO
-        }
-
-        public void ReadRemovePacketBody(BinaryReader reader) {
-            //TODO
-        }
-
         public void LoadTexture() {
-            if (core_texture_path_buff != null) {
-                Texture_Buff = ModLoader.GetTexture(core_texture_path_buff);
+            if (specific_texture_path != null) {
+                texture_index = (ushort)Textures.Count;
+                Textures.Add(ModLoader.GetTexture(specific_texture_path));
+            }
+        }
+
+        public Texture2D Texture() {
+            if (texture_index == TEXTURE_INDEX_NONE) {
+                return Utilities.Textures.TEXTURE_STATUS_DEFAULT;
             }
             else {
-                Texture_Buff = Utilities.Textures.TEXTURE_STATUS_DEFAULT;
+                return Textures[texture_index];
             }
         }
 
-        public bool HasDuration() {
-            return (!core_duration_instant && !core_duration_unlimited);
+        public void SetInstanceID(byte instance_id) {
+            Instance_ID = instance_id;
         }
 
-        //override with any specific effects
-        protected virtual void OnUpdate() { }
-        protected virtual void OnStart() { }
-        protected virtual void OnEnd() { }
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Shortcuts to Sync Data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-        protected double GetData(SYNC_DATA_TYPES key, double default_value = -1) {
-            double value = default_value;
-            if (!sync_data.TryGetValue(key, out value)) {
-                Utilities.Commons.Error("Status attempted to access invalid sync data: " + GetType() + " " + key);
-            }
-            return value;
+        /// <summary>
+        /// Update the time remaining string. Returns true if the string has changed.
+        /// </summary>
+        /// <returns></returns>
+        private bool UpdateTimeRemaining() {
+            //TODO
+            return false;
         }
 
-        public double Magitude { get { return GetData(SYNC_DATA_TYPES.MAGNITUDE); } }
+        protected void RemoveLocally() {
+            //TODO
+        }
 
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Static Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        protected void RemoveEverywhere() {
+            //TODO
+        }
+
+        protected void Add(int target_index, int owner_index, Dictionary<AUTOSYNC_DATA_TYPES, float> sync_data, bool target_is_player = true, bool owner_is_player = true) {
+            //TODO
+        }
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance Methods To Override ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        protected virtual void OnStart() {}
+
+        protected virtual void OnUpdate() {}
+
+        protected virtual void OnEnd() {}
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Example ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         public class Heal : Status {
             public Heal() : base(IDs.Heal) {
                 //any overwrites
-                core_display_name = "Heal"; //not needed unless displayed as a buff
-                core_texture_path_buff = "ExperienceAndClasses/Textures/Status/Heal"; //not needed unless displayed as a buff
+                specific_name = "Heal"; //not needed unless displayed as a buff
+                specific_texture_path = "ExperienceAndClasses/Textures/Status/Heal"; //not needed unless displayed as a buff
 
                 //add any sync data types that will be used (for syncing)
-                core_sync_data_types.Add(SYNC_DATA_TYPES.MAGNITUDE);
+                specific_autosync_data_types.Add(AUTOSYNC_DATA_TYPES.MAGNITUDE1);
             }
 
             //must inlcude a static add method with target/owner and any extra info
-            public static void Add(Player target, MPlayer owner, double magnitude) {
-                Add(target, owner, IDs.Heal, new Dictionary<SYNC_DATA_TYPES, double> {
-                    { SYNC_DATA_TYPES.MAGNITUDE, magnitude }
+            public static void CreateNew(Player target, MPlayer owner, float magnitude) {
+                Add(target, owner, IDs.Heal, new Dictionary<AUTOSYNC_DATA_TYPES, float> {
+                    { AUTOSYNC_DATA_TYPES.MAGNITUDE1, magnitude }
                 });
             }
 
@@ -342,9 +253,6 @@ namespace ExperienceAndClasses.Systems {
             protected override void OnUpdate() { }
             protected override void OnEnd() { }
         }
-
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Statuses ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 
     }
 }
