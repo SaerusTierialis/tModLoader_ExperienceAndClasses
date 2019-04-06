6d98d430a9e8e47025b5785384daf52a50dbcc40
SaerusTierialis <me12399@gmail.com>
2019-04-06 00:35:42 -0400

    ability

--- Systems/Ability.cs
+++ Systems/Ability.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
-using Terraria.ID
+using Terraria.ID;
 
 namespace ExperienceAndClasses.Systems {
     public abstract class Ability {
@@ -18,14 +18,18 @@ namespace ExperienceAndClasses.Systems {
             NONE, //leave this last
         }
 
-        public enum RESULT : byte {
+        public enum USE_RESULT : byte {
             SUCCESS,
             FAIL_NOT_ENOUGH_MANA,
+            FAIL_NOT_ENOUGH_RESOURCE,
             FAIL_ON_COOLDOWN,
             FAIL_LINE_OF_SIGH,
             FAIL_NO_TARGET,
+            FAIL_BUFF,
             FAIL_STATUS,
             FAIL_DEAD,
+            FAIL_CHANNELLING,
+            FAIL_SPECIFIC,
         }
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Auto-Populated Lookup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -102,13 +106,17 @@ namespace ExperienceAndClasses.Systems {
 
         public float[] Specific_Attribute_Multiplier_Magnitude { get; protected set; } = Enumerable.Repeat(1f, (byte)Systems.Attribute.IDs.NUMBER_OF_IDs).ToArray();
 
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance Vars Generic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance Vars Generic (between activations) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
         public IDs ID { get; private set; } = IDs.NONE;
 
         public ushort ID_num { get; private set; } = (ushort)IDs.NONE;
 
         public DateTime Time_Cooldown_End { get; protected set; } = DateTime.MinValue;
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Instance Vars Generic (within activation) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        private int mana_cost;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Core Constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
@@ -124,12 +132,53 @@ namespace ExperienceAndClasses.Systems {
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Public Instance Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+        public void Activate() {
+            //check if activation is allowed
+        }
 
+        public bool CanUse {
+            get {
+                return TryUse() == USE_RESULT.SUCCESS;
+            }
+        }
+
+        public int ManaCost {
+            get {
+                return 0;
+            }
+        }
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Private Instance Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+        private USE_RESULT TryUse() {
+            //mana
 
-        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Warrior ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+            //resource
+
+            //cooldown
+
+            //line of sight
+
+            /*
+            FAIL_NOT_ENOUGH_MANA,
+            FAIL_NOT_ENOUGH_RESOURCE,
+            FAIL_ON_COOLDOWN,
+            FAIL_LINE_OF_SIGH,
+            FAIL_NO_TARGET,
+            FAIL_BUFF,
+            FAIL_STATUS,
+            FAIL_DEAD,
+            FAIL_CHANNELLING,
+            FAIL_SPECIFIC,
+            */
+
+            //can use!
+            return USE_RESULT.SUCCESS;
+        }
+
+        /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Private Override Methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+            /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Warrior ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
         public class Block : Ability {
             public Block() : base(IDs.Block) {
 
