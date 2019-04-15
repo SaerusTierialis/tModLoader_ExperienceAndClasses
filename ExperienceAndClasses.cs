5b07c3b02cec7cceedba5d8bbbcb64f355d16d5f
SaerusTierialis <me12399@gmail.com>
2019-04-14 22:19:13 -0400

    beta v1.99.1

--- ExperienceAndClasses.cs
+++ ExperienceAndClasses.cs
@@ -19,7 +19,7 @@ namespace ExperienceAndClasses {
     class ExperienceAndClasses : Mod {
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Debug ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-        public static bool trace = true;
+        public static bool trace = false;
 
         /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Constants (and readonly) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
