44f3cc7ad64200ed58b62eda496e4c623917abc2
SaerusTierialis <me12399@gmail.com>
2019-11-27 22:34:46 -0500

    more class attribute clarification

--- Systems/PlayerClass.cs
+++ Systems/PlayerClass.cs
@@ -244,7 +244,7 @@ namespace ExperienceAndClasses.Systems {
                 }
 
                 //set tooltip
-                string tooltip_main = implementation_status_text + "\n\n" + Description + "\n\n" + recommended_weapon_text + "\n\nAttribute Bonus Per " + Attribute.LEVELS_PER_ATTRIBUTE_POINT_PER_STAR + " Levels:";
+                string tooltip_main = implementation_status_text + "\n\n" + Description + "\n\n" + recommended_weapon_text + "\n\nAttribute Bonus Per " + Attribute.LEVELS_PER_ATTRIBUTE_POINT_PER_STAR + " Levels (★ = 1 point):";
                 bool first = true;
                 string attribute_names = "";
                 foreach (byte id in Systems.Attribute.ATTRIBUTES_UI_ORDER) {
